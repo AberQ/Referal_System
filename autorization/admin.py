0f308e1f9ddf1ee88103c2c58f371b05dee5ea2a
AberQ <egor.master2017@gmail.com>
2024-12-02 21:51:27 +0500

    Сделал отдельную модель для обычных клиентов

--- autorization/admin.py
+++ autorization/admin.py
@@ -1,46 +1,20 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from django.utils.translation import gettext_lazy as _
-from .models import Admin
+from .models import Admin, ClientUser
 
+# Админка для модели Admin
+class AdminAdmin(admin.ModelAdmin):
+    list_display = ('email', 'is_active', 'is_staff', 'date_joined')  # Какие поля будут отображаться в списке
+    search_fields = ('email',)  # По каким полям можно будет искать
+    list_filter = ('is_active', 'is_staff')  # Фильтры для списка
+    ordering = ('date_joined',)  # Сортировка по умолчанию
 
-class CustomUserAdmin(UserAdmin):
-    # Поля, отображаемые в списке пользователей
-    list_display = ("email", "is_staff", "is_active", "date_joined")
-    list_filter = ("is_staff", "is_active", "is_superuser", "date_joined")
-    ordering = ("email",)
-    search_fields = ("email",)
+# Админка для модели ClientUser
+class ClientUserAdmin(admin.ModelAdmin):
+    list_display = ('phone_number', 'is_active', 'date_joined')  # Поля, отображаемые в списке
+    search_fields = ('phone_number',)  # Поиск по номеру телефона
+    list_filter = ('is_active',)  # Фильтры для списка
+    ordering = ('date_joined',)  # Сортировка по дате регистрации
 
-    # Поля для редактирования пользователя
-    fieldsets = (
-        (None, {"fields": ("email", "password")}),
-        (_("Personal info"), {"fields": ()}),  # Дополнительные персональные поля можно добавить здесь
-        (
-            _("Permissions"),
-            {
-                "fields": (
-                    "is_active",
-                    "is_staff",
-                    "is_superuser",
-                    "groups",
-                    "user_permissions",
-                )
-            },
-        ),
-        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
-    )
-
-    # Поля для создания нового пользователя
-    add_fieldsets = (
-        (
-            None,
-            {
-                "classes": ("wide",),
-                "fields": ("email", "password1", "password2", "is_staff", "is_active"),
-            },
-        ),
-    )
-
-
-# Регистрация модели и кастомного UserAdmin
-admin.site.register(Admin, CustomUserAdmin)
+# Регистрация моделей и их админок
+admin.site.register(Admin, AdminAdmin)
+admin.site.register(ClientUser, ClientUserAdmin)
--- autorization/models.py
+++ autorization/models.py
@@ -6,7 +6,19 @@ from django.core.mail import send_mail
 from django.db import models
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
+import unicodedata
 
+from django.conf import settings
+from django.contrib.auth import password_validation
+from django.contrib.auth.hashers import (
+    acheck_password,
+    check_password,
+    is_password_usable,
+    make_password,
+)
+from django.db import models
+from django.utils.crypto import salted_hmac
+from django.utils.translation import gettext_lazy as _
 
 class CustomUserManagerForAdmin(BaseUserManager):
 
@@ -77,3 +89,133 @@ class Admin(CustomAbstractUserForAdmin):
     class Meta(CustomAbstractUserForAdmin.Meta):
         swappable = "AUTH_USER_MODEL"
 
+class CustomAbstractBaseUser_Clients(models.Model):
+    last_login = models.DateTimeField(_("last login"), blank=True, null=True)
+
+    is_active = True
+
+    REQUIRED_FIELDS = []
+
+    # Stores the raw password if set_password() is called so that it can
+    # be passed to password_changed() after the model is saved.
+    
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.get_username()
+
+    # RemovedInDjango60Warning: When the deprecation ends, replace with:
+    # def save(self, **kwargs):
+    #   super().save(**kwargs)
+ 
+
+    def get_username(self):
+        """Return the username for this User."""
+        return getattr(self, self.USERNAME_FIELD)
+
+    def clean(self):
+        setattr(self, self.USERNAME_FIELD, self.normalize_username(self.get_username()))
+
+    def natural_key(self):
+        return (self.get_username(),)
+
+    @property
+    def is_anonymous(self):
+        """
+        Always return False. This is a way of comparing User objects to
+        anonymous users.
+        """
+        return False
+
+    @property
+    def is_authenticated(self):
+        """
+        Always return True. This is a way to tell if the user has been
+        authenticated in templates.
+        """
+        return True
+
+
+
+
+
+
+    
+
+
+  
+
+
+
+
+    def get_session_auth_hash(self):
+        """
+        Return an HMAC of the password field.
+        """
+        return self._get_session_auth_hash()
+
+    def get_session_auth_fallback_hash(self):
+        for fallback_secret in settings.SECRET_KEY_FALLBACKS:
+            yield self._get_session_auth_hash(secret=fallback_secret)
+
+    def _get_session_auth_hash(self, secret=None):
+        key_salt = "django.contrib.auth.models.AbstractBaseUser.get_session_auth_hash"
+        return salted_hmac(
+            key_salt,
+      
+            secret=secret,
+            algorithm="sha256",
+        ).hexdigest()
+
+    @classmethod
+    def get_email_field_name(cls):
+        try:
+            return cls.EMAIL_FIELD
+        except AttributeError:
+            return "email"
+
+    @classmethod
+    def normalize_username(cls, username):
+        return (
+            unicodedata.normalize("NFKC", username)
+            if isinstance(username, str)
+            else username
+        )
+
+
+
+class CustomUserManagerForClients(BaseUserManager):
+    """
+    Менеджер для создания обычных пользователей.
+    """
+    def create_user(self, phone_number, **extra_fields):
+        if not phone_number:
+            raise ValueError(_("Номер телефона обязателен"))
+        
+        user = self.model(phone_number=phone_number, **extra_fields)
+        user.save(using=self._db)
+        return user
+    
+
+class ClientUser(CustomAbstractBaseUser_Clients):
+    """
+    Модель для обычных пользователей, использующих номер телефона для аутентификации.
+    """
+
+    phone_number = models.CharField(_("номер телефона"), max_length=15, unique=True)
+    is_active = models.BooleanField(_("активен"), default=True)
+    date_joined = models.DateTimeField(_("дата регистрации"), default=timezone.now)
+
+    objects = CustomUserManagerForClients()
+
+    USERNAME_FIELD = "phone_number"  # Номер телефона будет использоваться для входа
+    REQUIRED_FIELDS = []  # Здесь нет обязательных полей, кроме phone_number
+
+    class Meta:
+        verbose_name = _("Обычный пользователь")
+        verbose_name_plural = _("Обычные пользователи")
+
+    def __str__(self):
+        return self.phone_number
