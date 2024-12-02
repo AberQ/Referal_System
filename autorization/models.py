ad31ded9fa7755a7dab905d6ec4684446488ecd7
AberQ <egor.master2017@gmail.com>
2024-12-02 21:23:31 +0500

    Вернул модели к использованию email, подготавливаю код к разделению пользователей на 2 категории(админы и пользователи)

--- autorization/models.py
+++ autorization/models.py
@@ -1,39 +1,37 @@
-from random import randint
 from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
-from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import *
+from django.contrib.auth.models import BaseUserManager
+from django.contrib.auth.validators import *
 from django.core.mail import send_mail
 from django.db import models
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
 
-class CustomBaseUserManager(models.Manager):
-    @classmethod
-    def normalize_phone_number(cls, phone_number):
-        """
-        Normalize the phone number by removing unnecessary characters.
-        """
-        return phone_number.replace(" ", "").replace("-", "").replace("(", "").replace(")", "")
-
-    def get_by_natural_key(self, phone_number):
-        return self.get(**{self.model.USERNAME_FIELD: phone_number})
-
-
-class CustomUserManager(CustomBaseUserManager):
-    def create_user(self, phone_number, password=None, **extra_fields):
-        if not phone_number:
-            raise ValueError("Пользователь должен указать номер телефона.")
-        phone_number = self.normalize_phone_number(phone_number)
-        
-        # Генерация 4-значного кода
-        confirmation_code = f"{randint(1000, 9999)}"
-        extra_fields.setdefault("confirmation_code", confirmation_code)
-
-        # Создаем пользователя с confirmation_code
-        user = self.model(phone_number=phone_number, confirmation_code=confirmation_code, **extra_fields)
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Пользователь должен указать адрес электронной почты.")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
-        user.save(using=self._db)  # Сохраняем пользователя с кодом подтверждения
+        user.save(using=self._db)
+        return user
 
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Суперпользователь должен иметь is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Суперпользователь должен иметь is_superuser=True.")
+
+       
+
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
         return user
 
 
@@ -42,53 +40,47 @@ class CustomAbstractUser(AbstractBaseUser, PermissionsMixin):
     An abstract base class implementing a fully featured User model with
     admin-compliant permissions.
 
-    Phone number and password are required. Other fields are optional.
+    Username and password are required. Other fields are optional.
     """
 
-    phone_number = models.CharField(
-        _("номер телефона"), max_length=15, unique=True, blank=False
-    )
+    email = models.EmailField(_("email address"), blank=False, unique=True)
     is_staff = models.BooleanField(
-        _("статус персонала"),
+        _("staff status"),
         default=False,
-        help_text=_("Обозначает, может ли пользователь входить в административный сайт."),
+        help_text=_("Designates whether the user can log into this admin site."),
     )
     is_active = models.BooleanField(
-        _("активный"),
+        _("active"),
         default=True,
         help_text=_(
-            "Обозначает, должен ли пользователь считаться активным. "
-            "Снимите этот флажок вместо удаления учетных записей."
+            "Designates whether this user should be treated as active. "
+            "Unselect this instead of deleting accounts."
         ),
     )
-    date_joined = models.DateTimeField(_("дата регистрации"), default=timezone.now)
+    date_joined = models.DateTimeField(_("date joined"), default=timezone.now)
 
     objects = CustomUserManager()
-
-    USERNAME_FIELD = "phone_number"
+    USERNAME_FIELD = "email"  
     REQUIRED_FIELDS = []
 
     class Meta:
-        verbose_name = _("пользователь")
-        verbose_name_plural = _("пользователи")
+        verbose_name = _("Авторизационник")
+        verbose_name_plural = _("Авторизационники")
         abstract = True
 
     def email_user(self, subject, message, from_email=None, **kwargs):
-        """Отправить email пользователю (если нужно оставить функционал)."""
-        send_mail(subject, message, from_email, [self.phone_number], **kwargs)
+        """Send an email to this user."""
+        send_mail(subject, message, from_email, [self.email], **kwargs)
 
 
 class CustomUser(CustomAbstractUser):
     """
-    Пользователи внутри системы аутентификации Django представлены этой моделью.
+    Users within the Django authentication system are represented by this
+    model.
+
+    Username and password are required. Other fields are optional.
     """
-    confirmation_code = models.CharField(
-        _("код подтверждения"),
-        max_length=4,
-        blank=True,
-        null=True,
-        help_text=_("4-значный код подтверждения для регистрации."),
-    )
 
     class Meta(CustomAbstractUser.Meta):
         swappable = "AUTH_USER_MODEL"
+
