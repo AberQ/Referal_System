5d1bd8e8e5c712a438fcc7b5b5d5476a8673e804
AberQ <egor.master2017@gmail.com>
2024-12-02 15:52:50 +0500

    Добавил возможность регистрации суперюзеров и обновил админ-панель

--- autorization/admin.py
+++ autorization/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    
+    list_display = ("phone_number", "is_staff", "is_active", "date_joined")
+   
+    search_fields = ("phone_number",)
+  
+    list_filter = ("is_staff", "is_active", "date_joined")
+
+   
+    fieldsets = (
+        (None, {"fields": ("phone_number", "password")}),
+        (_("Personal info"), {"fields": ()}),  
+        (_("Permissions"), {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("phone_number", "password1", "password2", "is_staff", "is_active"),
+        }),
+    )
+    ordering = ("phone_number",)
+
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- autorization/models.py
+++ autorization/models.py
@@ -1,16 +1,26 @@
-from django.db import models
 from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 from django.contrib.auth.models import PermissionsMixin
 from django.core.mail import send_mail
-from django.core.validators import RegexValidator
+from django.db import models
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
+class CustomBaseUserManager(models.Manager):
+    @classmethod
+    def normalize_phone_number(cls, phone_number):
+        """
+        Normalize the phone number by removing unnecessary characters.
+        """
+        return phone_number.replace(" ", "").replace("-", "").replace("(", "").replace(")", "")
 
-class CustomUserManager(BaseUserManager):
+    def get_by_natural_key(self, phone_number):
+        return self.get(**{self.model.USERNAME_FIELD: phone_number})
+    
+class CustomUserManager(CustomBaseUserManager):
     def create_user(self, phone_number, password=None, **extra_fields):
         if not phone_number:
             raise ValueError("Пользователь должен указать номер телефона.")
+        phone_number = self.normalize_phone_number(phone_number)
         user = self.model(phone_number=phone_number, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
@@ -25,59 +35,57 @@ class CustomUserManager(BaseUserManager):
         if extra_fields.get("is_superuser") is not True:
             raise ValueError("Суперпользователь должен иметь is_superuser=True.")
 
-        return self.create_user(phone_number, password, **extra_fields)
+        phone_number = self.normalize_phone_number(phone_number)
+        user = self.model(phone_number=phone_number, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
 
 class CustomAbstractUser(AbstractBaseUser, PermissionsMixin):
     """
-    Абстрактный класс для кастомной модели пользователя.
+    An abstract base class implementing a fully featured User model with
+    admin-compliant permissions.
+
+    Phone number and password are required. Other fields are optional.
     """
 
     phone_number = models.CharField(
-        _("номер телефона"),
-        max_length=15,
-        unique=True,
-        validators=[
-            RegexValidator(
-                regex=r"^\+?1?\d{9,15}$",
-                message=_(
-                    "Введите корректный номер телефона. "
-                    "Он должен содержать от 9 до 15 цифр."
-                ),
-            )
-        ],
+        _("номер телефона"), max_length=15, unique=True, blank=False
     )
     is_staff = models.BooleanField(
         _("статус персонала"),
         default=False,
-        help_text=_("Определяет, может ли пользователь входить в административный сайт."),
+        help_text=_("Обозначает, может ли пользователь входить в административный сайт."),
     )
     is_active = models.BooleanField(
         _("активный"),
         default=True,
         help_text=_(
-            "Определяет, следует ли считать этого пользователя активным. "
-            "Снимите галочку вместо удаления учетных записей."
+            "Обозначает, должен ли пользователь считаться активным. "
+            "Снимите этот флажок вместо удаления учетных записей."
         ),
     )
     date_joined = models.DateTimeField(_("дата регистрации"), default=timezone.now)
 
     objects = CustomUserManager()
+
     USERNAME_FIELD = "phone_number"  
-    REQUIRED_FIELDS = []  
+    REQUIRED_FIELDS = []
+
     class Meta:
-        verbose_name = _("Пользователь")
-        verbose_name_plural = _("Пользователи")
+        verbose_name = _("пользователь")
+        verbose_name_plural = _("пользователи")
         abstract = True
 
     def email_user(self, subject, message, from_email=None, **kwargs):
-        """Отправить письмо пользователю."""
+        """Отправить email пользователю (если нужно оставить функционал)."""
         send_mail(subject, message, from_email, [self.phone_number], **kwargs)
 
 
 class CustomUser(CustomAbstractUser):
     """
-    Модель для кастомного пользователя.
+    Пользователи внутри системы аутентификации Django представлены этой моделью.
     """
 
     class Meta(CustomAbstractUser.Meta):
